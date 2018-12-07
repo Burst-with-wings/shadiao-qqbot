25f017f459bdf253929e03488928ea97364fdc5a
Nosolution <32201220+Nosolution@users.noreply.github.com>
2018-12-07 19:44:47 +0800

    实现复读功能(大雾

--- config.py
+++ config.py
@@ -5,3 +5,4 @@ PORT = 8080
 
 SUPERUSERS = {1354034014, 1206985125}
 COMMAND_START.add('')
+NICKNAME = ['罗伯特', 'Robot', 'robot', '机器人']
--- plugins/base.py
+++ plugins/base.py
@@ -1,5 +1,7 @@
 from none import on_command, CommandSession, permission as perm
 from none.message import unescape
+from none import on_natural_language, NLPSession
+import random
 
 
 @on_command('echo', only_to_me=False)
@@ -17,3 +19,8 @@ async def _(session: CommandSession):
 async def inform(session: CommandSession):
     await session.send(session.get_optional('message') or session.current_arg)
 
+
+@on_natural_language()
+async def repeat(session: NLPSession):
+    i = random.random()-0.5
+    await session.send(session.msg.strip("吗?？")+("!" if i < 0 else ''))
