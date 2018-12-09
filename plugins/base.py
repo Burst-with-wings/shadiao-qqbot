8c8ac799f978be747ac45b30fb09e308aa1adeae
Nosolution <32201220+Nosolution@users.noreply.github.com>
2018-12-09 22:41:04 +0800

    feat: 查询天气

--- plugins/base.py
+++ plugins/base.py
@@ -2,6 +2,7 @@ from none import on_command, CommandSession, permission as perm
 from none.message import unescape
 from none import on_natural_language, NLPSession
 import random
+from function.weather import query_weather
 
 
 @on_command('echo', only_to_me=False)
@@ -20,7 +21,17 @@ async def inform(session: CommandSession):
     await session.send(session.get_optional('message') or session.current_arg)
 
 
+@on_command('天气', only_to_me=False)
+async def weather(session: CommandSession):
+    info = (session.get_optional('message') or session.current_arg).split(' ')
+    if len(info) == 1:
+        await session.send(query_weather(info[0]))
+    else:
+        await session.send(query_weather(info[0], info[1]))
+
+
 @on_natural_language()
 async def repeat(session: NLPSession):
     i = random.random()-0.5
-    await session.send(session.msg.strip("吗?？")+("!" if i < 0 else ''))
+    msg = session.msg.strip("么吧吗?？")+("!" if i < 0 else '')
+    await session.send(msg)
